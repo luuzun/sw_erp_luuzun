5ef85b50a1e27f0fafaae25a60977c0ca02ffbe3
luuzu <luuzu@luuzun>
2017-03-14 20:08:16 +0900

    SQL SessionFactory 초기화 완료

---
+++ src/test/java/kr/or/dgit/sw_erp_luuzun/MyBatisSqlSessionFactoryTest.java
@@ -0,0 +1,30 @@
+package kr.or.dgit.sw_erp_luuzun;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import kr.or.dgit.sw_erp_luuzun.util.MyBatisSqlSessionFactory;
+
+public class MyBatisSqlSessionFactoryTest {
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		sqlSessionFactory = MyBatisSqlSessionFactory.getSqlSessionFactory();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		//sqlSessionFactory = null;
+	}
+
+	@Test
+	public void testSqlSession() {
+		//Assert.assertNotNull(sqlSessionFactory.openSession());
+		System.out.println(sqlSessionFactory.openSession());
+	}
+
+}
