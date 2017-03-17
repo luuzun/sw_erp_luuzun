c01a31542ea6b85433b458d53700c8d58f0944f9
luuzun <luuzun@naver.com>
2017-03-17 21:45:46 +0900

    TT

--- src/main/java/kr/or/dgit/sw_erp_luuzun/util/MyBatisSqlSessionFactory.java
+++ src/main/java/kr/or/dgit/sw_erp_luuzun/util/MyBatisSqlSessionFactory.java
@@ -4,23 +4,27 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class MyBatisSqlSessionFactory {
 	private static SqlSessionFactory sqlSessionFactory;
-	
+
 	public static SqlSessionFactory getSqlSessionFactory() {
 		if (sqlSessionFactory==null){
 			InputStream inputStream;
 			try {
-				inputStream = Resources.getResourceAsStream("mybatis_config.xml");
+				inputStream = Resources.getResourceAsStream("mybatis-config.xml");
 				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 			} catch (IOException e) {
-				System.out.println("error");
 				e.printStackTrace();
 			}
 		}
 		return sqlSessionFactory;
+	}
+	
+	public static SqlSession openSession(){
+		return getSqlSessionFactory().openSession();
 	}
 }
